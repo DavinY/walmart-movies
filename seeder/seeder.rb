5b5041bcd17c254bbc6dffd495f421ce77dff509
Davin Y <davin@192.168.1.4>
2022-03-14 21:51:48 +0700

    fixing header in seeder file

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,21 +1,13 @@
 pages << {
     page_type: "listings",
     method: "GET",
-    headers: {
-        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding" => "gzip",
-        "content-security-policy" => "policy",
-        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-        "sec-ch-ua-mobile" => "?0",
-        "sec-ch-ua-full-version-list" => 'Not A;Brand";v="99.0.0.0", "Chromium";v="99.0.4844.51", "Google Chrome";v="99.0.4844.51"',
-        "sec-ch-ua-platform" => '"Linux"',
-        "sec-fetch-dest" => "image",
-        "sec-fetch-mode" => "no-cors",
-        "sec-fetch-site" => "cross-site",
-        "upgrade-insecure-requests" => "1",
-        "content-type" => "application/javascript; charset=utf-8",
-    },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
-    fetch_type: "browser"
+    headers: {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15",
+        "Accept-Language": "en-CA,en-US;q=0.9,en;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive"
+    }
 }
   
