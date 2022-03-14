8e063f665e6c6fd31113db817cad888a32248545
Davin Y <davin@192.168.1.4>
2022-03-14 21:10:22 +0700

    change header in seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,20 @@
 pages << {
     page_type: "listings",
     method: "GET",
-    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    headers: {
+        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding" => "gzip",
+        "content-security-policy" => "policy",
+        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+        "sec-ch-ua-mobile" => "?0",
+        "sec-ch-ua-full-version-list" => 'Not A;Brand";v="99.0.0.0", "Chromium";v="99.0.4844.51", "Google Chrome";v="99.0.4844.51"',
+        "sec-ch-ua-platform" => '"Linux"',
+        "sec-fetch-dest" => "image",
+        "sec-fetch-mode" => "no-cors",
+        "sec-fetch-site" => "cross-site",
+        "upgrade-insecure-requests" => "1",
+        "content-type" => "application/javascript; charset=utf-8",
+    },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     fetch_type: "browser"
 }
