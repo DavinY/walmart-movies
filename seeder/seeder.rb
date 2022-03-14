e8cae690d5f3c446e15120448dedbaf53e291977
Davin Y <davin@192.168.1.4>
2022-03-14 20:55:09 +0700

    edit header in seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,9 @@
 pages << {
     page_type: "listings",
     method: "GET",
-    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    headers: {
+        "user-agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+    },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     fetch_type: "browser"
 }
