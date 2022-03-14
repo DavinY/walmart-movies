65c253eeea2218be55d2512b8c01ec9415055158
Davin Y <davin@192.168.1.4>
2022-03-14 21:53:20 +0700

    add fetch_type

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,13 +1,14 @@
 pages << {
     page_type: "listings",
     method: "GET",
-    url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     headers: {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15",
         "Accept-Language": "en-CA,en-US;q=0.9,en;q=0.8",
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive"
-    }
+    },
+    url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
+    fetch_type: "browser",
 }
   
